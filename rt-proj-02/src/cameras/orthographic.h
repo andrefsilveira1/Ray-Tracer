559c684d1e16882f96596d0cde3d92b1e5b945bf
PauloVLB <vitorlimaborges@gmail.com>
2024-03-26 08:08:23 -0300

    Made camera.cpp and added needed methods in both types of Cameras

--- rt-proj-02/src/cameras/orthographic.h
+++ rt-proj-02/src/cameras/orthographic.h
@@ -9,6 +9,11 @@ class OrthographicCamera : public Camera {
     // ...
 };
 
+OrthographicCamera* create_orthographic_camera(
+    const ParamSet& ps_camera,
+    const ParamSet& ps_look_at, unique_ptr<Film>&& the_film
+);
+
 };
 
 #endif
