29a46a4a601ddcae790787296edf86fd88206c32
André Freitas <freitasandre38@gmail.com>
2024-04-09 17:41:13 -0300

    feat: create new class flat material derivated from material base class. this is gonna allow us create another classes derivated from material

--- rt-proj-03/src/core/material.h
+++ rt-proj-03/src/core/material.h
@@ -13,5 +13,16 @@ public:
 
 };
 
+class FlatMaterial : public Material {
+    public:
+    FlatMaterial(const Color& color): received_color(color) {}
+
+    Color color() const override {
+        return received_color;
+    }
+
+    private:
+    Color received_color;
+}
 
 #endif // MATERIAL_H
