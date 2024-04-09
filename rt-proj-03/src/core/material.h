fff42aadc729cd86c50973bb01233d14723b71c7
André Freitas <freitasandre38@gmail.com>
2024-04-09 17:37:59 -0300

    refactor: change material class to fit derived class flat material

--- rt-proj-03/src/core/material.h
+++ rt-proj-03/src/core/material.h
@@ -3,20 +3,15 @@
 
 #include <string>
 #include <glm/glm.hpp>
-
+#include "color.h"
 class Material {
 public:
     // Implement type ?
 
-    Material(const glm::vec3& color): rgb_color(color){}
+    ~Material() = default;
+    virtual Color color() const = 0;
 
-
-    glm::vec3 getColor() const {
-        return rgb_color;
-    }
-
-private:
-    glm::vec3 rgb_color;
 };
+
 
 #endif // MATERIAL_H
