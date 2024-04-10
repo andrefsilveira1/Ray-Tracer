fd11f398448ad8c611b4bf42b47e524c17eeb5d0
André Freitas <freitasandre38@gmail.com>
2024-04-10 18:41:23 -0300

    feat: create intersect method using delta

--- rt-proj-03/src/core/sphere.cpp
+++ rt-proj-03/src/core/sphere.cpp
@@ -4,4 +4,18 @@
 namespace rt3 {
     Sphere::Sphere(const Point3f& center, float r, const std::shared_ptr<FlatMaterial>& material)
     : center(center), r(r), material(material) {}
+
+    Sphere::~Sphere() {}
+
+    bool Sphere::intersect(const Ray& r, Surfel* sf) const {
+        return (glm::dot(oc, r.d) * glm::dot(oc, r.d)) - (glm::dot(r.d,r.d) * glm::dot(oc, oc) - s.r*s.r) >= 0;
+    }
+
+    bool Sphere::intersect_p(const Ray& r) const {
+        // TODO ?
+    }
+
+    const Material* Sphere::get_color() const {
+        return material.color();
+    }
 }
