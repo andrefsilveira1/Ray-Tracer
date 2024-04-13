51615680861738873dbdb9ab22d79fe48495569a
André Freitas <freitasandre38@gmail.com>
2024-04-13 16:26:34 -0300

    fix: solve problems with sampler and flat integrator. adjust imports too

--- rt-proj-03/src/integrators/flat.h
+++ rt-proj-03/src/integrators/flat.h
@@ -3,14 +3,15 @@
 
 #include "../core/integrator.h"
 #include "../core/camera.h"
+#include "sampler.h"
 
 namespace rt3 {
 
-class FlatIntegrator : public Integrator {
+class FlatIntegrator : public SamplerIntegrator {
 public:
-    FlatIntegrator(std::shared_ptr<const Camera> cam) : FlatIntegrator(cam) {}
+    FlatIntegrator(std::shared_ptr<const Camera> cam) : SamplerIntegrator(cam) {}
 
-    Color Li(const Ray& ray, const Scene& scene) const override;
+    Color Li(const Ray& ray, const Scene& scene) const; // This should override later
 };
 
 } // namespace rt3
--- rt-proj-03/src/integrators/sampler.h
+++ rt-proj-03/src/integrators/sampler.h
@@ -1,8 +1,8 @@
 #ifndef SAMPLER_INTEGRATOR_H
 #define SAMPLER_INTEGRATOR_H
 
-#include "integrator.h"
-#include "camera.h"
+#include "../core/integrator.h"
+#include "../core/camera.h"
 
 namespace rt3 {
 
