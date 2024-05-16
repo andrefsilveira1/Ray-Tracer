a85694dea40141a0cb36583464ce4e85e4ddd378
André Freitas <freitasandre38@gmail.com>
2024-05-16 10:44:02 -0300

    refactor: create sample li using unkown parameter

--- rt-proj-05/src/lights/directional.cpp
+++ rt-proj-05/src/lights/directional.cpp
@@ -2,20 +2,34 @@
 
 namespace rt3 {
 
-tuple<Color, Vector3f, unique_ptr<VisibilityTester>> DirectionalLight::sample_Li(
-  const shared_ptr<Surfel>& hit) {}
+tuple<Color, Vector3f, unique_ptr<VisibilityTester>> DirectionalLight::sample_Li(const shared_ptr<Surfel>& hit) {
+    Vector3f lightDirection = to - from; // Calculate the light dir. Should I put this as negative ?
+    lightDirection = glm::normalize(lightDirection);
+
+    shared_ptr<Surfel> lightSurfel = make_shared<Surfel>(
+        to, 
+        Vector3f(),
+        lightDirection,
+        std::numeric_limits<float>::infinity() // This really should be infinity or from light source
+    );
+
+    unique_ptr<VisibilityTester> visTester = std::make_unique<VisibilityTester>(hit, lightSurfel);
+
+    return std::make_tuple(color_int, lightDirection, std::move(visTester));
+
+}
 
 DirectionalLight* create_directional_light(const ParamSet& ps) {
-  return new DirectionalLight(retrieve(ps, "L", Color()),
-                              retrieve(ps, "scale", Vector3f()),
-                              retrieve(ps, "from", Vector3f()),
-                              retrieve(ps, "to", Vector3f()));
+    return new DirectionalLight(
+        retrieve(ps, "L", Color()),
+        retrieve(ps, "scale", Vector3f()),
+        retrieve(ps, "from", Point3f()),
+        retrieve(ps, "to", Point3f())
+    );
 }
 
 Vector3f DirectionalLight::normalize_light(const Vector3f& scl) {
-  Vector3f lightDir = normalize(-scl);
-
-  return lightDir;
+    return glm::normalize(-scl); // Maybe should replace the normalize lightDirection from sample_Li
 }
 
-}  // namespace rt3
+} // namespace rt3
--- rt-proj-05/src/lights/directional.h
+++ rt-proj-05/src/lights/directional.h
@@ -7,8 +7,8 @@ namespace rt3 {
 
 class DirectionalLight : public LightLi {
 public:
-  Vector3f from, to;
-  DirectionalLight(const Color& c, const Vector3f& scl, const Vector3f& from, const Vector3f& to)
+  Point3f from, to;
+  DirectionalLight(const Color& c, const Vector3f& scl, const Point3f& from, const Point3f& to)
       : LightLi(c, scl), from(from), to(to){};
   virtual void preprocess(const Scene&) {};
   Vector3f normalize_light(const Vector3f& scl);
