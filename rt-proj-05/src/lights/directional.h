5ac844c7f7b2d181edcda67b9af82942c01769f7
André Freitas <freitasandre38@gmail.com>
2024-05-17 09:15:50 -0300

    refactor: change directional.h

--- rt-proj-05/src/lights/directional.h
+++ rt-proj-05/src/lights/directional.h
@@ -9,12 +9,13 @@ class DirectionalLight : public LightLi {
 public:
   Vector3f lightDir;
   Point3f from;
-  DirectionalLight(const Color& c, const Vector3f& scl, const Vector3f& direction, Point3f& from)
-      : LightLi(c, scl), lightDir(direction), from(from){};
+  DirectionalHit bounds;
+  DirectionalLight(const Color& c, const Vector3f& scl, const Vector3f& direction, Point3f& from, const DirectionalHit& bounds)
+      : LightLi(c, scl), lightDir(direction), from(from), bounds(bounds){};
+
   virtual void preprocess(const Scene&) {};
   Vector3f normalize_light(const Vector3f& scl);
-  tuple<Color, Vector3f, unique_ptr<VisibilityTester>> sample_Li(
-    const shared_ptr<Surfel>& hit) override;
+  tuple<Color, Vector3f, unique_ptr<VisibilityTester>> sample_Li(const shared_ptr<Surfel>& hit) override;
 };
 
 DirectionalLight* create_directional_light(const ParamSet& ps);
